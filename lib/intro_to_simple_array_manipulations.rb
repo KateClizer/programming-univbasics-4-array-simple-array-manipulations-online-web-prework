501a41fb4513534004665a13fd5d5249aa44aa50
Katelynne Clizer <kateclizer@gmail.com>
2019-09-25 14:23:41 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, element)
+    colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+    next_color = "violet"
+   colors_in_the_rainbow.push(next_color)
+end  
+   
+  def using_unshift(array, elemant)
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    new_neighborhood = "Staten Island"
+    bouroughs_in_nyc.unshift(new_neighborhood)
+  end
+
+def using_pop(array)
+continents.pop
+end
+   
